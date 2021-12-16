324efbb50140b7d17d72366554111c551aee3aee
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2021-12-16 21:06:27 +0200

    Clicking usernames redirs to public profile page

--- frontend/src/components/PostDescCard.jsx
+++ frontend/src/components/PostDescCard.jsx
@@ -3,13 +3,16 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Stack from "react-bootstrap/Stack";
 import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 import { Navigate } from "react-router-dom";
 import { useState } from "react";
 import { DateTime } from "luxon";
 
 const PostDescCard = ({ post }) => {
   const [redirComponent, setRedirComponent] = useState(null);
-
+  /**
+   * Show only maximum of 200 first chars of the description
+   */
   let desc;
   if (post.description.length > 200) {
     desc = post.description.substring(0, 200) + "......";
@@ -21,9 +24,25 @@ const PostDescCard = ({ post }) => {
     setRedirComponent(<Navigate to={`/post/${post._id}`} />);
   };
 
+  const redirectToPublicProfile = (username) => {
+    setRedirComponent(<Navigate to={`/profile/${username}`} />);
+  };
+
   return (
     <Card className="mt-4 border" bg="dark" text="light">
-      <Card.Header as="h5">{post.createdBy.username}</Card.Header>
+      <Card.Header>
+        <Nav>
+          <Nav.Link
+            className="fs-4"
+            bsPrefix="themed-nav-link"
+            onClick={() => {
+              redirectToPublicProfile(post.createdBy.username);
+            }}
+          >
+            {post.createdBy.username}
+          </Nav.Link>
+        </Nav>
+      </Card.Header>
       <Card.Body>
         <Card.Title>{post.header}</Card.Title>
         <Card.Subtitle className="mb-3 text-muted">
