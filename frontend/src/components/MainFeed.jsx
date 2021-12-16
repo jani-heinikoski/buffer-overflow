1af927a4ff5e4a0d23d943c9acd4f2979b067d65
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2021-12-16 23:04:36 +0200

    Structure for deleting and editing posts from main feed

--- frontend/src/components/MainFeed.jsx
+++ frontend/src/components/MainFeed.jsx
@@ -2,9 +2,51 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Stack from "react-bootstrap/Stack";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import { Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import PostDescCard from "./PostDescCard";
+
+const SearchComponent = (setPosts) => {
+  const searchPosts = async (e) => {
+    e.preventDefault();
+    try {
+      const serverResponse = await fetch("/api/post/search", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: document.querySelector("#formBasicSearch").value,
+        }),
+      });
+      if (serverResponse.ok) {
+        const resJSON = await serverResponse.json();
+        if (resJSON.posts instanceof Array) {
+          setPosts(resJSON.posts);
+          return;
+        }
+      }
+    } catch (ex) {
+      console.error(ex);
+    }
+  };
+  return (
+    <Form className="mb-3">
+      <Stack direction="horizontal" gap={3}>
+        <Form.Group controlId="formBasicSearch">
+          <Form.Control type="text" placeholder="Search posts by header..." />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={searchPosts}>
+          Search
+        </Button>
+      </Stack>
+    </Form>
+  );
+};
 
 const MainFeed = () => {
   const [posts, setPosts] = useState([]);
@@ -60,6 +102,14 @@ const MainFeed = () => {
 
   return (
     <Container>
+      <Row>
+        <h1 style={{ textAlign: "center" }} className="mb-3">
+          All posts
+        </h1>
+        <hr style={{ color: "whitesmoke" }} />
+        {SearchComponent(setPosts)}
+        <hr style={{ color: "whitesmoke" }} />
+      </Row>
       <Row>
         <Col>{p}</Col>
       </Row>
--- frontend/src/components/PostDescCard.jsx
+++ frontend/src/components/PostDescCard.jsx
@@ -3,10 +3,70 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Stack from "react-bootstrap/Stack";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import { Navigate } from "react-router-dom";
 import { useState } from "react";
 import { DateTime } from "luxon";
+
+const getUser = () => {
+  let user;
+  try {
+    user = JSON.parse(window.localStorage.getItem("user"));
+  } catch {
+    user = null;
+  }
+  return user;
+};
+
+const DeleteButton = (post) => {
+  if (!post) {
+    return <></>;
+  }
+  const deletePost = () => {
+    console.log(`deleting post ${post.header}`);
+  };
+  let user = getUser();
+  if (user) {
+    if (user._id === post.createdBy._id) {
+      return (
+        <Button
+          variant="danger"
+          onClick={deletePost}
+          style={{ width: "auto", marginLeft: "auto" }}
+        >
+          Delete post
+        </Button>
+      );
+    }
+  }
+  return <></>;
+};
+
+const EditButton = (post) => {
+  if (!post) {
+    return <></>;
+  }
+  const editPost = () => {
+    console.log(`redirring to edit post ${post.header}`);
+  };
+  let user = getUser();
+  if (user) {
+    if (user._id === post.createdBy._id) {
+      return (
+        <Button
+          variant="primary"
+          onClick={editPost}
+          style={{ width: "auto", marginLeft: "16px" }}
+        >
+          Edit post
+        </Button>
+      );
+    }
+  }
+  return <></>;
+};
 
 const PostDescCard = ({ post }) => {
   const [redirComponent, setRedirComponent] = useState(null);
@@ -31,17 +91,27 @@ const PostDescCard = ({ post }) => {
   return (
     <Card className="mt-4 border" bg="dark" text="light">
       <Card.Header>
-        <Nav>
-          <Nav.Link
-            className="fs-4"
-            bsPrefix="themed-nav-link"
-            onClick={() => {
-              redirectToPublicProfile(post.createdBy.username);
-            }}
-          >
-            {post.createdBy.username}
-          </Nav.Link>
-        </Nav>
+        <Row>
+          <Col>
+            <Nav>
+              <Nav.Link
+                className="fs-4"
+                bsPrefix="themed-nav-link"
+                onClick={() => {
+                  redirectToPublicProfile(post.createdBy.username);
+                }}
+              >
+                {post.createdBy.username}
+              </Nav.Link>
+            </Nav>
+          </Col>
+          <Col>
+            <Row>
+              {DeleteButton(post)}
+              {EditButton(post)}
+            </Row>
+          </Col>
+        </Row>
       </Card.Header>
       <Card.Body>
         <Card.Title>{post.header}</Card.Title>
